b5224137598fe228349b4f8b62e8b10a14c6b931
Keshini <kdpatel8184@gmail.com>
2025-09-19 09:01:17 -0600

    Added  Home Page verifications

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,9 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
 });
 
 test('get started link', {tag: '@Sanity' } ,  async ({ page }) => {
--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -1,27 +1,56 @@
 import {test, expect} from "@playwright/test"
 
-test("Verify Home Page", async ({ page }) => {
-    await page.goto("https://practicesoftwaretesting.com/");
-    console.log("navigated to software testing practice homePage sucessfully");
-    await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
-    await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
+test.describe("Verify Home Page with No Auth ", () => {
 
+  test.beforeEach(async ({ page }) => {
+     await page.goto("https://practicesoftwaretesting.com/");
+     console.log("navigated to software testing practice homePage sucessfully");
+  });
+
+  test.afterEach(async ({ page }) => {
+     await page.close();  
+  });
+
+  test("visual test", async ({ page }) => {
+    await page.waitForLoadState("networkidle");
+    await expect(page).toHaveScreenshot("Home-page-no-auth.png");
+  }); 
+
+  test("Verify Home Page sign In Button", async ({ page }) => {    
+    await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+  });
+
+  test("Verify home Page title ", async ({ page }) => {  
+    await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
+  });
+
+  test("Verify Product grid ", async ({ page }) => {  
     const productGrid =  page.locator(".col-md-9");
     await expect(productGrid.getByRole("link")).toHaveCount(9);
+   });
 
+  test("Verify Product Thor Hammer", async ({ page }) => {
     await page.getByTestId("search-query").fill("Thor Hammer");
-    await page.getByTestId("search-submit").click();
-   
+    await page.getByTestId("search-submit").click();    
     const selectedProduct = page.getByAltText("Thor Hammer");
-    expect(selectedProduct).toBeVisible();
-    selectedProduct.click();
+    await expect(selectedProduct).toBeVisible();
+    await selectedProduct.click();
+  });
+});
 
+test.describe("Home Page customer 01 auth ", () => {
+  test.use ({storageState: ".auth/customer01.json" });
+  test.beforeEach(async ({page}) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+  });
 
+  test("visual test authorized ", async ({ page }) => {
+    await page.waitForLoadState("networkidle");
+    await expect (page).toHaveScreenshot("Home-page-customer01.png");
+  });
 
-
-
-
-
-
-
-})
+  test("Verify Customer 01 Signed in ", async ({page})=> {
+    await expect(page.getByTestId("nav-sign-in")).not.toBeVisible();  
+    await expect(page.locator('[data-test="nav-menu"]')).toContainText('Jane Doe');  
+  });
+});
